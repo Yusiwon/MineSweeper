83511084dd4340eb9d67ca5f9b6c3142c9249dd6
Yusiwon <danseryu@naver.com>
2021-03-07 20:09:49 +0900

    상태바와 상태를 출력할 공간까지 만들었다 버튼 만드는중

--- src/MineSweeper/MineGUI.java
+++ src/MineSweeper/MineGUI.java
@@ -1,20 +1,62 @@
 package MineSweeper;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MineGUI extends JFrame {
-    JMenuBar menuBar = new JMenuBar();
-    JMenu file = new JMenu("파일");
-    JMenuItem file_new = new JMenuItem("새 게임");
-    JMenuItem file_end = new JMenuItem("종료");
 
-    public MineGUI() {
-        this.setJMenuBar(menuBar);
+
+    public  JFrame createFrame() {
+
+        JFrame frame = new JFrame("MineSweeper");
+        //frame.setLocation(200, 400);
+        frame.setPreferredSize(new Dimension(400, 150));
+
+        frame.setJMenuBar((JMenuBar) createMenuBar());
+        frame.add(printState());
+        frame.add(makeButton());
+        frame.pack();
+        frame.setVisible(true);
+        return frame;
+    }
+
+    public Component createMenuBar() {
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu file = new JMenu("파일");
+        JMenuItem file_new = new JMenuItem("새 게임");
+        JMenuItem file_end = new JMenuItem("종료");
+
         menuBar.add(file);
         file.add(file_new);
         file.add(file_end);
-        this.setSize(50 * 8, 50 * 8); // 50-지뢰한칸의 사이즈, 8-지뢰의 갯수
-        this.setVisible(true);
+
+        return menuBar;
+    }
+
+    public JPanel printState() { // 남은지뢰수 등등 표시하는 칸
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(new JLabel("TEST"));
+
+        return panel;
+    }
+
+    public JPanel makeButton() { // 지뢰찾을 버튼 만들기
+        JPanel panel = new JPanel();
+        GridLayout grid = new GridLayout(8, 8);
+        setLayout(grid);
+        JButton[][] button = new JButton[8][8];
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                button[i][j] = new JButton();
+                panel.add(button[i][j]);
+                //add(button[i][j]);
+            }
+        }
+
+        return panel;
     }
 }
 
--- src/MineSweeper/MineMain.java
+++ src/MineSweeper/MineMain.java
@@ -1,7 +1,11 @@
 package MineSweeper;
 
+import javax.swing.*;
+
 public class MineMain {
     public static void main(String[] args) {
         MineGUI gui = new MineGUI();
+        gui.createFrame();
+
     }
 }
